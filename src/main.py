3089cf8eb6aaef90af4fb23757becf6a2d3b7d69
Jiří Ondráček <ondracekj23@spstrutnov.cz>
2024-01-09 09:37:14 +0100

    2 hrac + kolize

--- src/main.py
+++ src/main.py
@@ -18,8 +18,18 @@ velikost_hrace_sirka = 50
 
 
 pozice_hrace_x = 10
-pozice_hrace_y = 10
+pozice_hrace_y = 300
 rychlost_hrace = 0.3
+
+velikost_hrace_vyska2 = 120
+velikost_hrace_sirka2 = 50
+
+
+pozice_hrace_x2 = 740
+pozice_hrace_y2 = 300
+rychlost_hrace2 = 0.3
+
+
 
 while True:
     for udalost in pygame.event.get():
@@ -29,34 +39,51 @@ while True:
     
     stisknute_klavesy = pygame.key.get_pressed()
     
-    if stisknute_klavesy[pygame.K_UP]:
+    if stisknute_klavesy[pygame.K_w]:
         pozice_hrace_y -= rychlost_hrace
         
-    if stisknute_klavesy[pygame.K_DOWN]:
+    if stisknute_klavesy[pygame.K_s]:
         pozice_hrace_y += rychlost_hrace
+        
+    if stisknute_klavesy[pygame.K_UP]:
+        pozice_hrace_y2 -= rychlost_hrace2
+        
+    if stisknute_klavesy[pygame.K_DOWN]:
+        pozice_hrace_y2 += rychlost_hrace2
     
     
     pozice_micku_x += rychlost_micku_x
     pozice_micku_y += rychlost_micku_y
     
     if pozice_micku_x < 0:
-        pozice_micku_x = 0
-        rychlost_micku_x *= -1
+        print("you lost")
+        print("you lost")
+        print("you lost")
+        print("you lost")
+        quit()
     if pozice_micku_y < 0:
         pozice_micku_y = 0
         rychlost_micku_y *= -1
-        
+    
+    hrac2_rect = pygame.Rect(pozice_hrace_x2, pozice_hrace_y2, velikost_hrace_sirka2, velikost_hrace_vyska2)
     hrac_rect = pygame.Rect(pozice_hrace_x, pozice_hrace_y, velikost_hrace_sirka, velikost_hrace_vyska)
     micek_rect = pygame.Rect(pozice_micku_x, pozice_micku_y, velikost_micku, velikost_micku)
 
     if hrac_rect.colliderect(micek_rect):
         rychlost_micku_x *= -1
         rychlost_micku_y *= -1
-        
+    if hrac2_rect.colliderect(micek_rect):
+        rychlost_micku_x *= -1
+        rychlost_micku_y *= -1
+    
     
     if pozice_micku_x > rozliseni_okna[0] - velikost_micku:
-        pozice_micku_x = rozliseni_okna[0] - velikost_micku
-        rychlost_micku_x *= -1
+        print("you lost")
+        print("you lost")
+        print("you lost")
+        print("you lost")
+        quit()
+    
     if pozice_micku_y > rozliseni_okna[1] - velikost_micku:
         pozice_micku_y = rozliseni_okna[1] - velikost_micku
         rychlost_micku_y *= -1
@@ -65,5 +92,7 @@ while True:
     
     pygame.draw.rect(okno, (0, 0, 0), (pozice_hrace_x, pozice_hrace_y, velikost_hrace_sirka,velikost_hrace_vyska))
     pygame.draw.ellipse(okno, (0, 0, 255), (pozice_micku_x, pozice_micku_y, velikost_micku, velikost_micku))
+    pygame.draw.rect(okno, (0, 0, 0), (pozice_hrace_x2, pozice_hrace_y2, velikost_hrace_sirka2,velikost_hrace_vyska2))
+    
     
     pygame.display.update()
