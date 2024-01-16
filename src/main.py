35e13f60aafe56bfde8121f61bbdcd884cf4db0c
Wollfiksk <ondracekj23@spstrutnov.cz>
2024-01-16 07:53:07 +0100

    UI + reset a quit tlacitko

--- src/main.py
+++ src/main.py
@@ -33,12 +33,23 @@ pozice_hrace_x2 = 740
 pozice_hrace_y2 = 300
 rychlost_hrace2 = 0.3
 
+button_rect2 = pygame.Rect(300, 200, 200, 100)
+button_rect = pygame.Rect(300, 330, 200, 100)
+button_color = (100, 100, 100)
+click_color = (100, 100, 100)
+
 font = pygame.font.SysFont(None , 40)
 font2 = pygame.font.SysFont(None , 40)
 esc = False
 
 fps = str(int(clock.get_fps()))
 
+def draw_button(rect, color, text):
+    pygame.draw.rect(okno, color, rect)
+    text_surface = font.render(text, True, (255, 255, 255))
+    text_rect = text_surface.get_rect(center=rect.center)
+    okno.blit(text_surface, text_rect)
+    
 def draw_text(x, y):
     txtimg = font2.render("Skore: " + str(skore), True, (0, 0, 0))
     okno.blit(txtimg, (x, y))
@@ -48,6 +59,12 @@ def fps_counter():
     fps_t = font.render(fps , 1, pygame.Color("RED"))
     okno.blit(fps_t,(0,0))
 
+def reset_game():
+    global pozice_micku_x, pozice_micku_y, skore
+    pozice_micku_x = 400
+    pozice_micku_y = 300
+    skore = 0
+
 while True:
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT:
@@ -56,6 +73,44 @@ while True:
         elif udalost.type == pygame.KEYDOWN:
             if udalost.key == pygame.K_ESCAPE:
                 esc = not esc
+        elif udalost.type == pygame.MOUSEBUTTONDOWN:
+            if udalost.button == 1:
+                if button_rect.collidepoint(udalost.pos):
+                    print("reset")
+                    reset_game()
+        elif udalost.type == pygame.MOUSEBUTTONDOWN:
+            if udalost.button == 1:
+                if button_rect2.collidepoint(udalost.pos):
+                    print("Quited")
+                    quit()
+        
+    
+    if esc:
+        is_hovered = button_rect.collidepoint(pygame.mouse.get_pos())
+        is_hovered2 = button_rect2.collidepoint(pygame.mouse.get_pos())
+        
+        pygame.draw.rect(okno, (0, 0, 0), (100, 100, 600, 400))
+        if is_hovered:
+            if pygame.mouse.get_pressed()[0]:  # Left mouse button is pressed
+                draw_button(button_rect, click_color, "Reseted")
+            else:
+                draw_button(button_rect, button_color, "Reset")
+        else:
+            draw_button(button_rect, button_color, "Reset")
+
+        if is_hovered2:
+            if pygame.mouse.get_pressed()[0]:  # Left mouse button is pressed
+                draw_button(button_rect2, click_color, "Quited")
+                quit()
+            else:
+                draw_button(button_rect2, button_color, "Quit")
+        else:
+            draw_button(button_rect2, button_color, "Quit")
+            
+        pygame.display.flip()
+        
+        
+    
     if not esc:            
         stisknute_klavesy = pygame.key.get_pressed()
         
